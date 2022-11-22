434c0e475a74487666a3702874f008bc6878284b
Abdelrhman Hani <abdohani362@gmail.com>
2022-11-23 01:12:06 +0200

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure ullam deserunt 
+            ea maxime repudiandae repellendus quaerat provident enim iste id?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure ullam deserunt 
+            ea maxime repudiandae repellendus quaerat provident enim iste id?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0 ;
+    }
+</style>
